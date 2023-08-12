d15dc04e99a3811d4f833e9dcd53b6a655061a79
Serednia Anastasia <anastastasiaseredna.gmail.com>
2023-08-12 16:00:48 +0300

    date 12/08/23

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,63 @@ $(function () {
         autoplaySpeed: 2000
       });
 
-      var mixer = mixitup('.categories__content');
-});
+      $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+          $('.filter-price__from').text(data.from);
+          $('.filter-price__to').text(data.to);
+    
+        },
+        onChange: function (data) {
+          $('.filter-price__from').text(data.from);
+          $('.filter-price__to').text(data.to);
+    
+        },
+    
+      });
+      
+      $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        vertical: true,
+        draggable: false
+      });
+    
+      $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+      });
+      
+      $('.select-style, .product-one__num').styler();
+
+      $(".star").rateYo({
+        starWidth: "11px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        numStars: 5,
+        readOnly: true,
+        spacing: "8px",
+        starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px"height="16px" viewBox="0 0 18 16" version="1.1"><g id="surface1"><path  d="M 8.101562 0.554688 L 6.0625 4.695312 L 1.496094 5.359375 C 0.679688 5.476562 0.351562 6.488281 0.945312 7.066406 L 4.246094 10.285156  L 3.464844 14.832031 C 3.324219 15.652344 4.191406 16.269531 4.914062 15.882812 L 9 13.738281 L 13.085938 15.882812   C 13.808594 16.265625 14.675781 15.652344 14.535156 14.832031 L 13.753906 10.285156 L 17.054688 7.066406 C 17.648438  6.488281 17.320312 5.476562 16.503906 5.359375 L 11.9375 4.695312 L 9.898438 0.554688 C 9.53125 -0.179688 8.472656 -0.191406  8.101562 0.554688 Z M 8.101562 0.554688 " /></g></svg>'
+      });
+
+      
+      // var mixer = mixitup('.categories__content');
+      // var mixer1 = mixitup('.design__content');
+      
+      var containerEl1 = document.querySelector('[data-ref="container-1"]');
+      var containerEl2 = document.querySelector('[data-ref="container-2"]');
+      var config = {
+        controls: {
+          scope: 'local'
+        }
+      };
+      
+      var mixer1 = mixitup(containerEl1, config);
+      var mixer2 = mixitup(containerEl2, config);
+      
+    });
--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,13 @@ function scripts(){
     [
        'node_modules/jquery/dist/jquery.js',
        'node_modules/slick-carousel/slick/slick.js', //Display multiple slick image slider and carousel using shortcode with category.
-      // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js', //Также fancyBox позволяет группировать картинки, создавая галереи,
-      // 'node_modules/rateyo/src/jquery.rateyo.js', //рейтингами звезд
+       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js', //Также fancyBox позволяет группировать картинки, создавая галереи,
+      'node_modules/rateyo/src/jquery.rateyo.js', //рейтингами звезд
        'node_modules/mixitup/dist/mixitup.js', 
        'node_modules/mixitup/dist/mixitup.min.js',
        //рейтингами звезд
-      // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js', //Плагин ion.rangeSlider  динамически менять шаг ползунка?
-      // 'node_modules/jquery-form-styler/dist/jquery.formstyler.js', //плагин для стилизации селектов, чекбоксов, радиокнопок, файловых и числовых полей
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js', //Плагин ion.rangeSlider  динамически менять шаг ползунка?
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js', //плагин для стилизации селектов, чекбоксов, радиокнопок, файловых и числовых полей
       'app/js/main.js'
     ])  
      .pipe(concat('main.min.js'))
