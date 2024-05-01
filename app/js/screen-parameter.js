6563bef0f45cd8bf0f37deb154e0ed96a6be57fb
Serednii Mykola <seredniimykola@gmail.com>
2024-05-01 08:37:02 +0200

    final chang

--- app/js/screen-parameter.js
+++ app/js/screen-parameter.js
@@ -1,14 +1,14 @@
-$(function () {
-  function screenParameter() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    $('.display-parametr').text(`${width}  ${height}`);
-  }
+// $(function () {
+//   function screenParameter() {
+//     const width = window.innerWidth;
+//     const height = window.innerHeight;
+//     $('.display-parametr').text(`${width}  ${height}`);
+//   }
 
-  screenParameter()
-  window.addEventListener('resize', (e) => {
-    screenParameter()
-  });
+//   screenParameter()
+//   window.addEventListener('resize', (e) => {
+//     screenParameter()
+//   });
 
-});
+// });
 
